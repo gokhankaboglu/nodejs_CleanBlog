719750828d7c449f8315cdbe31f4c5e8ef16b99a
Gökhan Kaboğlu <kaboglugokhan@gmail.com>
2021-05-28 16:45:01 +0300

    clean blog ejs

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 const port = 3000;
