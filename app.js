02f15768030c12594255dfa3f30a548d30c7d92b
Gökhan Kaboğlu <kaboglugokhan@gmail.com>
2021-05-28 22:42:20 +0300

    bug fixed

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Post = require('./models/Post');
 
 const app = express();
+
+//Connect DB
+mongoose.connect('mongodb://localhost/clean-blog-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template Engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+//Routes
+app.get('/', async(req, res) => {
+  const posts = await Post.find({})
+  res.render('index', {
+    posts:posts
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -22,7 +36,14 @@ app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
 
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
+
+console.log();
