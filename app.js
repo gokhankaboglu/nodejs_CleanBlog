b937e84187a6aabd530f7ea9228e5e8be76562d3
Gökhan Kaboğlu <kaboglugokhan@gmail.com>
2021-06-03 20:50:49 +0300

    MVC update

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
-const path = require('path');
-const Post = require('./models/Post');
+const methodOverride = require('method-override');
+const postController = require('./controllers/postControllers');
+const pageController = require('./controllers/pageControllers');
 
 const app = express();
 
@@ -10,6 +11,7 @@ const app = express();
 mongoose.connect('mongodb://localhost/clean-blog-db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 //Template Engine
@@ -19,34 +21,22 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //Routes
-app.get('/', async(req, res) => {
-  const posts = await Post.find({})
-  res.render('index', {
-    posts:posts
-  });
-});
+app.get('/', postController.getAllPosts);
+app.get('/posts/:id', postController.getPost);
+app.post('/posts', postController.createPost);
+app.put('/posts/:id', postController.updatePost);
+app.delete('/posts/:id', postController.deletePost);
 
-app.get('/posts/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id)
-  res.render('post', {
-    post
-  })
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-
-app.post('/posts', async (req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/posts/edit/:id', pageController.getEditPage);
 
 const port = 3000;
 app.listen(port, () => {
